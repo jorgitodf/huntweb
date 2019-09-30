a18e3944c1f07d39937d26578732be1c60615723
Jorgito Paiva <jspaiva.a977@gmail.com>
2019-09-30 09:32:12 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Header from './componentes/Header'
+import api from './services/api';
+import Main from './pages/main';
+import Routes from './routes';
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Header />
+    <Routes />
+  </div>
+);
+
+/* class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello Rocketseat</h1>
+      </div>
+    )
+  }
+} */
 
 export default App;
